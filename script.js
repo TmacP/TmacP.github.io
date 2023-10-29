6f413f230dc042c2455a44692e75970c7e0828ec
beatburritochef <beatburritochef@gmail.com>
2023-10-29 11:44:28 -0600

    generalised the script.js

--- script.js
+++ script.js
@@ -1,51 +1,17 @@
-const togglePlayerButton = document.getElementById('togglePlayerButton');
-const toggleFormButton = document.getElementById('toggleFormButton');
-const toggleLinkButton = document.getElementById('toggleLinkButton');
-const toggleAboutButton = document.getElementById('toggleAboutButton');
-const toggleGameButton = document.getElementById('toggleGameButton'); // Add this line
+function toggleContainer(button, container) {
+    button.addEventListener('click', () => {
+        if (container.style.display === 'none') {
+            greetingContainer.style.display = 'none';
+            container.style.display = 'block';
+        } else {
+            container.style.display = 'none';
+            greetingContainer.style.display = 'block';
+        }
+    });
+}
 
-const playerContainer = document.getElementById('playerContainer');
-const formContainer = document.getElementById('formContainer');
-const linkContainer = document.getElementById('linkContainer');
-const aboutContainer = document.getElementById('aboutContainer');
-const gameContainer = document.getElementById('gameContainer'); // Add this line
-
-togglePlayerButton.addEventListener('click', () => {
-    if (playerContainer.style.display === 'none') {
-        playerContainer.style.display = 'block';
-    } else {
-        playerContainer.style.display = 'none';
-    }
-});
-
-toggleFormButton.addEventListener('click', () => {
-    if (formContainer.style.display === 'none') {
-        formContainer.style.display = 'block';
-    } else {
-        formContainer.style.display = 'none';
-    }
-});
-
-toggleLinkButton.addEventListener('click', () => {
-    if (linkContainer.style.display === 'none') {
-        linkContainer.style.display = 'block';
-    } else {
-        linkContainer.style.display = 'none';
-    }
-});
-
-toggleAboutButton.addEventListener('click', () => {
-    if (aboutContainer.style.display === 'none') {
-        aboutContainer.style.display = 'block';
-    } else {
-        aboutContainer.style.display = 'none';
-    }
-});
-
-toggleGameButton.addEventListener('click', () => { // Add this event listener
-    if (gameContainer.style.display === 'none') {
-        gameContainer.style.display = 'block';
-    } else {
-        gameContainer.style.display = 'none';
-    }
-});
+toggleContainer(togglePlayerButton, playerContainer);
+toggleContainer(toggleFormButton, formContainer);
+toggleContainer(toggleLinkButton, linkContainer);
+toggleContainer(toggleAboutButton, aboutContainer);
+toggleContainer(toggleGameButton, gameContainer);
