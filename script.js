84b9505132bd1cddd20ed436fec5f8e3e2ac1c2f
BeatBurritoChef <trm003@mail.usask.ca>
2024-05-23 12:10:52 -0600

    landing container pop in and outw

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 function toggleContainer(button, container, scriptToLoad) {
     button.addEventListener('click', () => {
-        if (container.style.display === 'none') {
-// make container display none so only the open one is visabile
-	playerContainer.style.display = 'none';
-    	formContainer.style.display = 'none';
-    	linkContainer.style.display = 'none';
-    	aboutContainer.style.display = 'none';
-	// set the one selected to block            
-	container.style.display = 'block';
-	// Scroll to the container 
-	container.scrollIntoView({ behavior: 'smooth' });
+        if (container.style.display === 'block') { // Check if the container is already displayed
+            // Set the landing container to block
+            landingContainer.style.display = 'block';
+            playerContainer.style.display = 'none';
+            formContainer.style.display = 'none';
+            linkContainer.style.display = 'none';
+            aboutContainer.style.display = 'none';
+        } else if (container.style.display === 'none') {
+            // Hide all other containers
+            playerContainer.style.display = 'none';
+            formContainer.style.display = 'none';
+            linkContainer.style.display = 'none';
+            aboutContainer.style.display = 'none';
+            // Hide the landing container
+            landingContainer.style.display = 'none';
 
+            // Set the selected container to block
+            container.style.display = 'block';
+
+            // Scroll to the container 
+            container.scrollIntoView({ behavior: 'smooth' });
 
             // Check if the script should be loaded
             if (scriptToLoad) {
@@ -19,6 +29,7 @@ function toggleContainer(button, container, scriptToLoad) {
                 document.head.appendChild(script);
             }
         } else {
+            // Hide the container if it's already displayed
             container.style.display = 'none';
         }
     });
@@ -28,3 +39,4 @@ toggleContainer(togglePlayerButton, playerContainer);
 toggleContainer(toggleFormButton, formContainer);
 toggleContainer(toggleLinkButton, linkContainer);
 toggleContainer(toggleAboutButton, aboutContainer);
+
