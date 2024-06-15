b0f94e497d34c74ac3a3e3d6076db3a8afe73601
beatburritochef <beatburritochef@gmail.com>
2024-06-15 14:01:57 -0600

    for now happy with colours. removed js file put inline

--- script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('year').innerHTML = new Date().getFullYear();
-    document.getElementById('contactForm').addEventListener('submit', handleSubmit);
-});
