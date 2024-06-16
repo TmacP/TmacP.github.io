6eec84649be66d9ed316f4f0d1972106119c8897
beatburritochef <beatburritochef@gmail.com>
2024-06-16 13:33:20 -0600

    messing around

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('year').innerHTML = new Date().getFullYear();
+    document.getElementById('contactForm').addEventListener('submit', handleSubmit);
+    });
