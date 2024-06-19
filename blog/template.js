a18e7f75e80cf1c605f3fdaac0672340e09f04b8
beatburritochef <beatburritochef@gmail.com>
2024-06-18 18:49:56 -0600

    update blog

--- blog/template.js
+++ blog/template.js
@@ -1,18 +1,21 @@
-// Load header template from external file
-fetch('header-template.html')
-    .then(response => response.text())
-    .then(html => {
-        // Create a temporary container
-        const tempContainer = document.createElement('div');
-        tempContainer.innerHTML = html.trim();
+ // Load header template from external file
+ fetch('header-template.html')
+ .then(response => response.text())
+ .then(html => {
+     // Create a temporary container
+     const tempContainer = document.createElement('div');
+     tempContainer.innerHTML = html.trim();
 
-        // Get the header element from the temporary container
-        const header = tempContainer.querySelector('header');
-        
-        // Insert the header into the header container in index.html
-        const headerContainer = document.getElementById('header-container');
-        headerContainer.appendChild(header);
-    })
-    .catch(error => {
-        console.error('Error fetching header template:', error);
-    });
+     // Get the template element from the temporary container
+     const template = tempContainer.querySelector('template');
+
+     // Clone the template content
+     const header = document.importNode(template.content, true);
+
+     // Insert the header into the header container
+     const headerContainer = document.getElementById('header-container');
+     headerContainer.appendChild(header);
+ })
+ .catch(error => {
+     console.error('Error fetching header template:', error);
+ });
